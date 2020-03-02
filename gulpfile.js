5a72d53758f6f96cfad46e189ea94e1db5c591a9
Спартак Турченко <4golf4@rambler.ru>
2020-03-02 23:21:18 +0300

    include gulp & make gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -28,14 +28,4 @@ function minifyCSS() {
   );
 }
 
-var gulp = require('gulp');
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
- 
-gulp.task('default', function () {
-    gulp.src('src/**/*.css')
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist'));
-    });
-    
+
