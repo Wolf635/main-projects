db30efbfa1530ceafcd2d7de05e739d664832a24
Спартак Турченко <4golf4@rambler.ru>
2020-03-02 21:57:22 +0300

    include gulp & make gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,23 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+const gulp = require("gulp");
+const cleanCSS = require("gulp-clean-css");
+
+function minifyCSS() {
+  return (
+    gulp
+      .src("./css/*.css")
+      .pipe(cleanCSS())
+      .pipe(gulp.dest("minified"))
+  );
+}
+
+gulp.task("minify-css", minifyCSS);
+
+gulp.task("watch", () => {
+  gulp.watch("./css/*.css", minifyCSS);
+});
+
+gulp.task('default', gulp.series('minify-css', 'watch'));
