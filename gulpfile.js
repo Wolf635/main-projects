9dd513285f9dd806dab5a08eeca0a25b87d426c7
Спартак Турченко <4golf4@rambler.ru>
2020-03-09 10:58:43 +0300

    refact: minor fixed main.css in main-project

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const {src,dest,watch} = require("gulp");
 const browserSync =  require('browser-sync').create();
 const sass = require("gulp-sass");
+const autoprefixer = require('gulp-autoprefixer');
 
 // Static server
  function bs() {
@@ -21,8 +22,22 @@ const sass = require("gulp-sass");
  function serveSass() {
     return src("./sass/**/*.sass", "./sass/**/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
   };
+
+  var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+ 
+gulp.task('default', function () {
+    gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+});
 
 exports.serve = bs;
