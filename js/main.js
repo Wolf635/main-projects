14dc2f581ae72a57c116e4ed60f2162549abae1e
Wolf635 <60838718+Wolf635@users.noreply.github.com>
2020-03-21 01:40:03 +0300

    feat: _interface.sass, main.js, index.html files

--- js/main.js
+++ js/main.js
@@ -50,4 +50,91 @@ $(document). ready(function () {
 
   new WOW().init();
 
+  // Валидация формы
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      //  строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+        email: true
+      }
+    }, // сообщения 
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух букв",
+        maxlength: "Имя может содержать не более 15 символов"
+      },
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Обязательно укажите email",
+        email: "Введите корректный email: name@domain.com"
+      }
+    } 
+  });
+
+  // маска для телефона
+  $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+
+  // Форма Онлайн контроль
+  $('.control__form').validate({
+    errorClass: "invalid",
+    rules: {
+      //  строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+      }
+    }, // сообщения 
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух букв",
+        maxlength: "Имя не более 15 символов"
+      },
+      userPhone: "Телефон обязателен",
+    } 
+  });
+
+// Footer форма
+
+  $('.footer__form').validate({
+    errorClass: "invalid",
+    rules: {
+      //  строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+      }
+    }, // сообщения 
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух букв",
+        maxlength: "Имя не более 15 символов"
+      },
+      userPhone: "Телефон обязателен",
+    } 
+  });
+
 });
