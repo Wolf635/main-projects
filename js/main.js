3c36471567ea0c2b05750e183f1f729b4f28ae86
Wolf635 <60838718+Wolf635@users.noreply.github.com>
2020-05-17 23:34:19 +0300

    feat: in index.html, _client.sass, main.js

--- js/main.js
+++ js/main.js
@@ -324,6 +324,12 @@ $("body").on('click', '[href*="#team"]', function(e){
   $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
   e.preventDefault();
 });
+// Прокрутка до client 
+$("body").on('click', '[href*="#client"]', function(e){
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
 // Прокрутка до contacts
 $("body").on('click', '[href*="#contacts"]', function(e){
   var fixed_offset = 100;
